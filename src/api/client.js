149cd37e1eed5c63c99ed57777c7ed60b992f904
Manjunath <manjunathgs908@gmail.com>
2026-05-31 14:00:32 +0530

    Hardcode backend URL

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
- baseURL: import.meta.env.VITE_API_URL || '/api',
+ baseURL: 'https://medifleet-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
